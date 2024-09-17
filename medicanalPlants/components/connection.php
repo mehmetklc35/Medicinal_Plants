23cccf9d970ee57b73259e92bdbc5c0507b67abf
Your Name <you@example.com>
2024-09-17 23:54:53 +0300

    17.09.2024 tarihinde projeye giriş yapıldı.....

---
+++ medicanalPlants/components/connection.php
@@ -0,0 +1,17 @@
+<?php
+  $db_name= 'mysql:host=localhost;dbname=shop_db';
+  $db_user = 'root';
+  $db_password = '';
+
+  $conn = new PDO($db_name, $db_user, $db_password);
+  
+  function unique_id() {
+    $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLM';
+    $charLength = strlen($chars);
+    $randomString = '';
+    for ($i = 0; $i < 20; $i++) {
+      $randomString.= $chars[mt_rand(0, $charLength - 1)];
+    }
+    return $randomString;
+  }
+?>
