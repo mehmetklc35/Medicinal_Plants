a0e1f451e0b57104645b12b9ff106a6180008250
Your Name <you@example.com>
2024-09-20 00:08:13 +0300

    20.09.2024 00:08 projeye devam edildi....

--- home.php
+++ home.php
@@ -17,66 +17,200 @@
       <?php include 'components/header.php'; ?>
       <div class="main">
             <?php include 'components/footer.php'; ?>
-            <div class="home-section">
-                  <div class="slider__slider slide1">
-                        <div class="overlay"></div>
-                        <div class="slide-detail">
-                              <h1>Lorem İpsum dolor sit</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                              <a href="view_products.php" class="btn">shop now</a>
+            <section class="home-section">
+                  <div class="slider">
+                        <div class="slider__slider slide1">
+                              <div class="overlay"></div>
+                              <div class="slide-detail">
+                                    <h1>Lorem İpsum dolor sit</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                              <div class="hero-dec-top"></div>
+                              <div class="hero-dec-bottom"></div>
                         </div>
-                        <div class="hero-dec-top"></div>
-                        <div class="hero-dec-bottom"></div>
-                  </div>
-                  <!-- slide end -->
-                  <div class="slider__slider slide2">
-                        <div class="overlay"></div>
-                        <div class="slide-detail">
-                              <h1>welcome to my shop</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                              <a href="view_products.php" class="btn">shop now</a>
+                        <!-- slide end -->
+                        <div class="slider__slider slide2">
+                              <div class="overlay"></div>
+                              <div class="slide-detail">
+                                    <h1>welcome to my shop</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                              <div class="hero-dec-top"></div>
+                              <div class="hero-dec-bottom"></div>
                         </div>
-                        <div class="hero-dec-top"></div>
-                        <div class="hero-dec-bottom"></div>
-                  </div>
-                  <!-- slide end -->
-                  <div class="slider__slider slide3">
-                        <div class="overlay"></div>
-                        <div class="slide-detail">
-                              <h1>Lorem İpsum dolor sit</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                              <a href="view_products.php" class="btn">shop now</a>
+                        <!-- slide end -->
+                        <div class="slider__slider slide3">
+                              <div class="overlay"></div>
+                              <div class="slide-detail">
+                                    <h1>Lorem İpsum dolor sit</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                              <div class="hero-dec-top"></div>
+                              <div class="hero-dec-bottom"></div>
                         </div>
-                        <div class="hero-dec-top"></div>
-                        <div class="hero-dec-bottom"></div>
-                  </div>
-                  <!-- slide end -->
-                  <div class="slider__slider slide4">
-                        <div class="overlay"></div>
-                        <div class="slide-detail">
-                              <h1>Lorem İpsum dolor sit</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                              <a href="view_products.php" class="btn">shop now</a>
+                        <!-- slide end -->
+                        <div class="slider__slider slide4">
+                              <div class="overlay"></div>
+                              <div class="slide-detail">
+                                    <h1>Lorem İpsum dolor sit</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                              <div class="hero-dec-top"></div>
+                              <div class="hero-dec-bottom"></div>
                         </div>
-                        <div class="hero-dec-top"></div>
-                        <div class="hero-dec-bottom"></div>
-                  </div>
-                  <!-- slide end -->
-                  <div class="slider__slider slide5">
-                        <div class="overlay"></div>
-                        <div class="slide-detail">
-                              <h1>Lorem İpsum dolor sit</h1>
-                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                              <a href="view_products.php" class="btn">shop now</a>
+                        <!-- slide end -->
+                        <div class="slider__slider slide5">
+                              <div class="overlay"></div>
+                              <div class="slide-detail">
+                                    <h1>Lorem İpsum dolor sit</h1>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                              <div class="hero-dec-top"></div>
+                              <div class="hero-dec-bottom"></div>
                         </div>
-                        <div class="hero-dec-top"></div>
-                        <div class="hero-dec-bottom"></div>
-                  </div>
-                  <!-- slide end -->
-                   <div class="left-arrow"><i class="bx bxs-left-arrow"></i></div>
-                   <div class="right-arrow"><i class="bx bxs-right-arrow"></i></div>
-            </div>
+                        <!-- slide end -->
+                        <div class="left-arrow"><i class="bx bxs-left-arrow"></i></div>
+                        <div class="right-arrow"><i class="bx bxs-right-arrow"></i></div>
+                  </div>             
+            </section>           
             <!-- home slider end -->
+            <section class="thumb">
+                  <div class="box-container">
+                        <div class="box">
+                              <img src="img/thumb2.jpg">
+                              <h3>green tea</h3>
+                              <p>Lorem ipsum dolor sit amet</p>
+                              <i class="bx bx-chevron-right"></i>
+                        </div>
+                        <div class="box">
+                              <img src="img/thumb0.jpg">
+                              <h3>lemon tea</h3>
+                              <p>Lorem ipsum dolor sit amet</p>
+                              <i class="bx bx-chevron-right"></i>
+                        </div>
+                        <div class="box">
+                              <img src="img/thumb1.jpg">
+                              <h3>green coffee</h3>
+                              <p>Lorem ipsum dolor sit amet</p>
+                              <i class="bx bx-chevron-right"></i>
+                        </div>
+                        <div class="box">
+                              <img src="img/thumb.jpg">
+                              <h3>green tea</h3>
+                              <p>Lorem ipsum dolor sit amet</p>
+                              <i class="bx bx-chevron-right"></i>
+                        </div>
+                  </div>
+            </section>
+            <section class="container">
+                  <div class="box-container">
+                        <div class="box">
+                              <img src="img/download.png">
+                              <span>healty tea</span>
+                              <h1>save up to 50% off</h1>
+                              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.ratione </p>
+                        </div>
+                  </div>
+            </section>
+            <section class="shop">
+                  <div class="title">
+                        <img src="img/download.png">
+                        <h1>Trending Products</h1>
+                  </div>
+                  <div class="row">
+                        <img src="img/about.jpg">
+                        <div class="row-detail">
+                              <img src="img/basil.jpg">
+                              <div class="top-footer">
+                                    <h1>a cup of green tea makes you healthy</h1>
+                              </div>
+                        </div>
+                  </div>
+                  <div class="box-container">
+                        <div class="box">
+                              <img src="img/card.jpg">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                        <div class="box">
+                              <img src="img/card0.jpg">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                        <div class="box">
+                              <img src="img/card1.jpg">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                        <div class="box">
+                              <img src="img/card2.jpg">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                        <div class="box">
+                              <img src="img/10.jpg">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                        <div class="box">
+                              <img src="img/6.webp">
+                              <a href="view_products.php" class="btn">shop now</a>
+                        </div>
+                  </div>
+            </section>
+            <section class="shop-category">
+                  <div class="box-container">
+                        <div class="box">
+                              <img src="img/6.jpg">
+                              <div class="detail">
+                                    <span>BIG OFFERS</span>
+                                    <h1>Extra 15% off</h1>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                        </div>
+                        <div class="box">
+                              <img src="img/7.jpg">
+                              <div class="detail">
+                                    <span>new in taste</span>
+                                    <h1>coffee house</h1>
+                                    <a href="view_products.php" class="btn">shop now</a>
+                              </div>
+                        </div>
+                  </div>
+            </section>
+            <section class="services">
+                  <div class="box-container">
+                        <div class="box">
+                              <img src="img/icon2.png">
+                              <div class="detail">
+                                    <h3>great savings</h3>
+                                    <p>save big every order</p>
+                              </div>
+                        </div>
+                        <div class="box">
+                              <img src="img/icon1.png">
+                              <div class="detail">
+                                    <h3>24*7 support</h3>
+                                    <p>one-on-one support</p>
+                              </div>
+                        </div>
+                        <div class="box">
+                              <img src="img/icon0.png">
+                              <div class="detail">
+                                    <h3>gift vouchers</h3>
+                                    <p>vouchers on every festivals</p>
+                              </div>
+                        </div>
+                        <div class="box">
+                              <img src="img/icon.png">
+                              <div class="detail">
+                                    <h3>worldwide delivery</h3>
+                                    <p>dropship worldwide</p>
+                              </div>
+                        </div>
+                  </div>
+            </section>
+            <?php include 'components/footer.php'; ?>
       </div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
       <script src="script.js"></script>
--- script.js
+++ script.js
@@ -16,3 +16,51 @@ userBtn.addEventListener('click', function() {
       let userBox = document.querySelector('.user-box');
       userBox.classList.toggle('active');
 })
+/*-----------home page slider-----------*/
+"use strict"
+const leftArrow = document.querySelector('.left-arrow .bxs-left-arrow'),
+      rightArrow = document.querySelector('.right-arrow .bxs-right-arrow'),
+      slider = document.querySelector('.slider');
+/*-----------scroll to right-----------*/
+function scrollRight(){
+      if (slider.scrollWidth - slider.clientWidth === slider.scrollLeft) {
+            slider.scrollTo({
+                  left: 0,
+                  behavior: "smooth"
+            });
+      }else{
+            slider.scrollBy({
+                  left: window.innerWidth,
+                  behavior: "smooth"
+            })
+      }
+}
+/*-----------scroll to left-----------*/
+function scrollLeft(){
+      slider.scrollBy({
+            left: -window.innerWidth,
+            behavior: "smooth"
+      });
+}
+let timerId = setInterval(scrollRight, 7000);
+
+/*-----------reset timer to scroll right-----------*/
+function resetTimer() {
+      clearInterval(timerId);
+      timerId = setInterval(scrollRight, 7000);
+}
+
+/*-----------scroll event-----------*/
+slider.addEventListener('click', function(ev) {
+      if(ev.target === leftArrow) {
+            scrollLeft();
+            resetTimer();
+      }
+})
+
+slider.addEventListener('click', function(ev) {
+      if(ev.target === rightArrow) {
+            scrollRight();
+            resetTimer();
+      }
+});
